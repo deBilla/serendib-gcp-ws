32a75e7a7b8446f370f0de22a98c3334e0a41920
debilla <dimuthu.billa@gmail.com>
2023-01-11 16:16:17 +0800

    first

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAW91_GATtsp1V61F8SOd5YktSXMmzrsJU",
+  authDomain: "serendib-ws.firebaseapp.com",
+  projectId: "serendib-ws",
+  storageBucket: "serendib-ws.appspot.com",
+  messagingSenderId: "586237173336",
+  appId: "1:586237173336:web:1f4c0c11547a141c2fb42d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
